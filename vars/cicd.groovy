9c2e7c7d201d7f0346860922dcab2b826f23a0a6
Surja07 <153250264+Surja07@users.noreply.github.com>
2025-08-16 00:03:50 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -27,6 +27,6 @@ def test(jobname)
 
 def release(jobname, ip) 
 {
-sh "scp '/var/lib/jenkins/workspace/${jobname}/HelloWorld.java' ubuntu@${ip}:/home/ubuntu/demo3"
+sh "scp '/var/lib/jenkins/workspace/${jobname}/HelloWorld.java' ubuntu@${ip}:/home/ubuntu/demo6"
 }
 
