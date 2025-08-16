42c6f2079355d061c748b1e625ada5f51ea1ad0d
Surja07 <153250264+Surja07@users.noreply.github.com>
2025-08-16 12:02:13 +0530

    Create cicdd.groovy

---
+++ vars/cicdd.groovy
@@ -0,0 +1,25 @@
+def git download ('repo')
+{
+  git branch: 'main', url: "https://github.com/Surja07/${repo}.git"
+}
+def mvn build()
+{
+  echo "mvn build package"
+}
+def deployment (jobname,ip) 
+{
+  sh "scp '/var/lib/jenkins/workspace/${jobname}/HelloWorld.java' ubuntu@${ip}:/home/ubuntu/demo3"
+}
+
+def test(jobname) 
+{
+    sh """
+        javac '${env.WORKSPACE}/HelloWorld.java'
+        java -cp '${env.WORKSPACE}' HelloWorld > '${env.WORKSPACE}/test_output.txt'
+    """
+}
+
+def release (jobname,ip) 
+{
+  sh "scp '/var/lib/jenkins/workspace/${jobname}/HelloWorld.java' ubuntu@${ip}:/home/ubuntu/demo3"
+}
